50acabd6e2e4c28bd85c9cedfe7fdb91a4b3fa7b
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2024-11-02 23:24:36 +0100

    Navbar component created

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+
+const Navbar = () => {
+  const { status } = useSession();
+
+  if (status === "loading") {
+    return (
+      <nav className="bg-white shadow-lg relative z-10">
+        <div className="px-20 py-4 flex justify-between items-center">
+          <Link href="/posts" className="text-blue-700 text-2xl font-black">
+            BlogPlatform
+          </Link>
+        </div>
+      </nav>
+    );
+  }
+
+  return (
+    <nav className="bg-white shadow-lg relative z-10">
+      <div className="px-20 py-4 flex justify-between items-center">
+        <Link href="/posts" className="text-blue-700 text-2xl font-black">
+          BlogPlatform
+        </Link>
+
+        <div className="flex items-center space-x-8">
+          <Link href="/posts" className="text-blue-600 hover:text-blue-800">
+            Posts
+          </Link>
+          {status === "authenticated" ? (
+            <>
+              <Link
+                href="/posts/new"
+                className="text-blue-600 hover:text-blue-800"
+              >
+                Create Post
+              </Link>
+              <button
+                onClick={() => signOut()}
+                className="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700"
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <button
+              onClick={() => signIn()}
+              className="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700"
+            >
+              Login
+            </button>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
