0c4a9ab13b31af0257eb280010ec61ba555eca9b
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2024-11-03 20:48:12 +0100

    Adding new post

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,17 +1,52 @@
 import { getPost } from "@/utils/api";
+import Image from "next/image";
+import { Post as PostType } from "@/types/post";
 
 interface PostProps {
   params: Promise<{ slug: string }>;
 }
 
-const Post = async ({ params }: PostProps) => {
+const PostDetails = async ({ params }: PostProps) => {
   const { slug } = await params;
+  const post: PostType | null = await getPost(slug);
 
-  const post = await getPost(slug);
+  if (!post) {
+    return (
+      <div className="text-center text-gray-500 py-10">Post not found.</div>
+    );
+  }
 
-  console.log(post, params);
-
-  return <div>Post</div>;
+  return (
+    <div className="flex flex-col items-center min-h-screen w-full py-32 px-10">
+      <div className="flex flex-col md:flex-row w-3/4">
+        <div className="w-full md:w-1/2 h-auto relative">
+          <Image
+            src={
+              post.image ||
+              "https://images.pexels.com/photos/28216688/pexels-photo-28216688/free-photo-of-autumn-camping.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            }
+            alt={post.title}
+            layout="fill"
+            objectFit="cover"
+            className="max-h-72 min-h-56"
+          />
+        </div>
+        <div className="flex flex-col p-6 w-full md:w-1/2">
+          <h1 className="text-3xl font-bold text-blue-700 mb-2">
+            {post.title}
+          </h1>
+          <p className="text-gray-600 mb-4">{post.description}</p>
+          <div className="text-gray-500 text-sm mb-4">
+            <span>Posted by {post.userEmail}</span> •{" "}
+            <span>{new Date(post.createdAt).toLocaleDateString()}</span>
+          </div>
+        </div>
+      </div>
+      <div className="w-3/4 mt-28 text-gray-700 prose prose-lg">
+        {post.content}
+      </div>
+    </div>
+  );
 };
 
-export default Post;
+export default PostDetails;
--- src/app/posts/new/page.tsx
+++ src/app/posts/new/page.tsx
@@ -1,16 +1,26 @@
 "use client";
 
 import { createPost } from "@/utils/api";
-import React from "react";
+import React, { useState } from "react";
 
 const CreatePost = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    content: "",
+    image: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
   const handleCreatePost = async () => {
     try {
-      const title = "T Title";
-      const content = "This is a sample content.";
-      const description = "Desc";
-      const image = "https://example.com/sample-image.jpg";
-
+      const { title, description, content, image } = formData;
       const newPost = await createPost(title, description, content, image);
       console.log("Post created successfully:", newPost);
     } catch (error) {
@@ -19,8 +29,107 @@ const CreatePost = () => {
   };
 
   return (
-    <div>
-      <button onClick={handleCreatePost}>Create Post</button>
+    <div className="flex justify-center items-center h-screen bg-white">
+      <div className="bg-white p-8 shadow-xl w-full max-w-xl">
+        <h2 className="text-2xl font-bold mb-12 text-center text-blue-700">
+          Create a New Post
+        </h2>
+
+        <div className="relative mb-4">
+          <input
+            type="text"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            required
+            className={`peer h-10 mb-5 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-blue-500 bg-transparent ${
+              formData.title ? "py-6" : ""
+            }`}
+            placeholder=" "
+          />
+          <label
+            className={`absolute left-0 top-2 text-gray-500 text-sm transition-all pointer-events-none ${
+              formData.title
+                ? "text-blue-500 -top-3.5"
+                : "peer-focus:text-blue-500"
+            }`}
+          >
+            Title (Required)
+          </label>
+        </div>
+
+        <div className="relative mb-4">
+          <input
+            type="text"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            className={`peer h-10 w-full mb-5 border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-blue-500 bg-transparent ${
+              formData.description ? "py-6" : ""
+            }`}
+            placeholder=" "
+          />
+          <label
+            className={`absolute left-0 top-2 text-gray-500 text-sm transition-all pointer-events-none ${
+              formData.description
+                ? "text-blue-500 -top-3.5"
+                : "peer-focus:text-blue-500"
+            }`}
+          >
+            Description (Optional)
+          </label>
+        </div>
+
+        <div className="relative mb-6">
+          <input
+            type="url"
+            name="image"
+            value={formData.image}
+            onChange={handleChange}
+            className={`peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-blue-500 bg-transparent ${
+              formData.image ? "py-6" : ""
+            }`}
+            placeholder=" "
+          />
+          <label
+            className={`absolute left-0 top-2 text-gray-500 text-sm transition-all pointer-events-none ${
+              formData.image
+                ? "text-blue-500 -top-3.5"
+                : "peer-focus:text-blue-500"
+            }`}
+          >
+            Image URL (Optional)
+          </label>
+        </div>
+        <div className="relative mb-4">
+          <textarea
+            name="content"
+            value={formData.content}
+            onChange={handleChange}
+            required
+            className={`peer h-32 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-blue-500 bg-transparent ${
+              formData.content ? "py-6" : ""
+            }`}
+            placeholder=" "
+          />
+          <label
+            className={`absolute left-0 top-2 text-gray-500 text-sm transition-all pointer-events-none ${
+              formData.content
+                ? "text-blue-500 -top-3.5"
+                : "peer-focus:text-blue-500"
+            }`}
+          >
+            Content (Required)
+          </label>
+        </div>
+
+        <button
+          onClick={handleCreatePost}
+          className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition duration-200"
+        >
+          Create Post
+        </button>
+      </div>
     </div>
   );
 };
