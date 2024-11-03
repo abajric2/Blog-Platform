dda7b12494fcac11e85acc413a1abce07078d96e
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2024-11-04 00:34:24 +0100

    Minor design changes

--- src/app/posts/new/page.tsx
+++ src/app/posts/new/page.tsx
@@ -29,8 +29,8 @@ const CreatePost = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-white">
-      <div className="bg-white p-8 shadow-xl w-full max-w-xl">
+    <div className="flex justify-center items-center h-screen bg-white mt-10">
+      <div className="bg-white py-12 px-16 shadow-xl w-full max-w-2xl">
         <h2 className="text-2xl font-bold mb-12 text-center text-blue-700">
           Create a New Post
         </h2>
@@ -48,10 +48,8 @@ const CreatePost = () => {
             placeholder=" "
           />
           <label
-            className={`absolute left-0 top-2 text-gray-500 text-sm transition-all pointer-events-none ${
-              formData.title
-                ? "text-blue-500 -top-3.5"
-                : "peer-focus:text-blue-500"
+            className={`absolute left-0 top-2 text-gray-500 text-sm transition-all pointer-events-none peer-focus:text-blue-500 ${
+              formData.title && "-top-5"
             }`}
           >
             Title (Required)
@@ -70,10 +68,8 @@ const CreatePost = () => {
             placeholder=" "
           />
           <label
-            className={`absolute left-0 top-2 text-gray-500 text-sm transition-all pointer-events-none ${
-              formData.description
-                ? "text-blue-500 -top-3.5"
-                : "peer-focus:text-blue-500"
+            className={`absolute left-0 top-2 text-gray-500 text-sm transition-all pointer-events-none peer-focus:text-blue-500 ${
+              formData.description && "-top-5"
             }`}
           >
             Description (Optional)
@@ -86,16 +82,14 @@ const CreatePost = () => {
             name="image"
             value={formData.image}
             onChange={handleChange}
-            className={`peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-blue-500 bg-transparent ${
+            className={`peer h-10 w-full border-b-2 mb-5 border-gray-300 text-gray-900 focus:outline-none focus:border-blue-500 bg-transparent ${
               formData.image ? "py-6" : ""
             }`}
             placeholder=" "
           />
           <label
-            className={`absolute left-0 top-2 text-gray-500 text-sm transition-all pointer-events-none ${
-              formData.image
-                ? "text-blue-500 -top-3.5"
-                : "peer-focus:text-blue-500"
+            className={`absolute left-0 top-2 text-gray-500 text-sm transition-all pointer-events-none peer-focus:text-blue-500 ${
+              formData.image && "-top-5"
             }`}
           >
             Image URL (Optional)
@@ -107,16 +101,14 @@ const CreatePost = () => {
             value={formData.content}
             onChange={handleChange}
             required
-            className={`peer h-32 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-blue-500 bg-transparent ${
+            className={`peer h-32 w-full border-b-2 mb-5 border-gray-300 text-gray-900 focus:outline-none focus:border-blue-500 bg-transparent ${
               formData.content ? "py-6" : ""
             }`}
             placeholder=" "
           />
           <label
-            className={`absolute left-0 top-2 text-gray-500 text-sm transition-all pointer-events-none ${
-              formData.content
-                ? "text-blue-500 -top-3.5"
-                : "peer-focus:text-blue-500"
+            className={`absolute left-0 top-2 text-gray-500 text-sm transition-all pointer-events-none peer-focus:text-blue-500 ${
+              formData.content && "-top-5"
             }`}
           >
             Content (Required)
